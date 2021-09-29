44cbbd54ea9f626d6787f735031d279b16bb4240
Daniel Levy <danaylevy2004@gmail.com>
2021-09-29 13:06:03 -0700

    finished rgb

--- image.py
+++ image.py
@@ -24,8 +24,7 @@ def image_formatter(img, img_type):
 def image_data(path=Path("static/assets/"), img_list=None):  # path of static images is defaulted
     if img_list is None:  # color_dict is defined with defaults
         img_list = [
-#            {'source': "danielscomputer", 'label': "Daniel is the Best Scrum Master", 'file': "DanielPhotograph.PNG"},
-            {'source': "iconsdb.com", 'label': "White square", 'file': "white-square-16.png"},
+         {'source': "danielscomputer", 'label': "Daniel is the Best Scrum Master", 'file': "DanielPhotograph.PNG"},
         ]
     # gather analysis data and meta data for each image, adding attributes to each row in table
     for img_dict in img_list:
