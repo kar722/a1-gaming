52e6dfd3a58055fb82b725fd5c00ee9f7b04ff38
Daniel Levy <danaylevy2004@gmail.com>
2021-10-06 12:12:30 -0700

    binary rgb is done thank you kar kar

--- main.py
+++ main.py
@@ -133,6 +133,10 @@ def Samsung():
 def xbox():
     return render_template("xbox.html")
 
+@app.route('/binary RGB 2/')
+def binaryRGB2():
+    return render_template("binary RGB 2.html")
+
 @app.route('/Laptops/')
 def Laptops():
     return render_template("Laptops.html")
