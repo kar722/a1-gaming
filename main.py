cc446a19f4effdb31beb2b5bf47bee8c6b5ce779
WilliamDu22 <cloud1563@gmail.com>
2021-11-08 12:55:19 -0800

    monitors again

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from pathlib import Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
 import json, requests
 
-
 # create a Flask instance
 app = Flask(__name__)
 
